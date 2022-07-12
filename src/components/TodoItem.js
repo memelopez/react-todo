b484d7e165405afb6f56022302dc8ba51e3cca69
memelopez <meme30lopez@gmail.com>
2022-07-12 17:19:54 -0600

    Add inner delete function, bind it and call props delete function from it. Add delete button and onClick event

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -5,6 +5,7 @@ class TodoItem extends React.Component {
   constructor(props) {
     super(props);
     this.handleOnChange = this.handleOnChange.bind(this);
+    this.deleteTodo = this.deleteTodo.bind(this);
   }
 
   handleOnChange() {
@@ -12,11 +13,26 @@ class TodoItem extends React.Component {
     handleChangeProps(todo.id);
   }
 
+  deleteTodo() {
+    const { todo, deleteTodoProps } = this.props;
+    deleteTodoProps(todo.id);
+  }
+
   render() {
     const { todo } = this.props;
     return (
       <li>
-        <input type="checkbox" checked={todo.completed} onChange={this.handleOnChange} />
+        <input
+          type="checkbox"
+          checked={todo.completed}
+          onChange={this.handleOnChange}
+        />
+        <button
+          type="button"
+          onClick={this.deleteTodo}
+        >
+          Delete
+        </button>
         {todo.title}
       </li>
     );
@@ -30,6 +46,7 @@ TodoItem.propTypes = {
     completed: PropTypes.bool.isRequired,
   }).isRequired,
   handleChangeProps: PropTypes.func.isRequired,
+  deleteTodoProps: PropTypes.func.isRequired,
 };
 
 export default TodoItem;
