88cc21dab77fe25aaa0aaded1d002cc2750b00b7
memelopez <meme30lopez@gmail.com>
2022-07-13 15:22:33 -0600

    Add uuid as id's to hardcoded Todo's and to addTodoItem()

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,4 +1,7 @@
 import React from 'react';
+
+import { v4 as uuidv4 } from 'uuid';
+
 import TodosList from './TodosList';
 import Header from './Header';
 import InputTodo from './InputTodo';
@@ -9,17 +12,17 @@ class TodoContainer extends React.Component {
     this.state = {
       todos: [
         {
-          id: 1,
+          id: uuidv4(),
           title: 'Setup development environment',
           completed: true,
         },
         {
-          id: 2,
+          id: uuidv4(),
           title: 'Develop website and add content',
           completed: false,
         },
         {
-          id: 3,
+          id: uuidv4(),
           title: 'Deploy to live server',
           completed: false,
         },
@@ -53,7 +56,7 @@ class TodoContainer extends React.Component {
   addTodoItem = (title) => {
     const { todos } = this.state;
     const newTodo = {
-      id: 4,
+      id: uuidv4(),
       title: title,
       completed: false,
     };
